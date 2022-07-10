c11d6d5fe3bd05b829ed7510dd28496c32616fd2
kaushalendra-pandey <67643722+kaushalendra-pandey@users.noreply.github.com>
2022-07-10 19:17:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const para = document.createElement("p");
 const node = document.createTextNode("This is new.");
 
-const ele = document.getElementById("time)
+const ele = document.getElementById("time")
 ele.appendChild(node);
