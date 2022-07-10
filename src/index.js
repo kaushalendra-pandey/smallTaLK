6809e730124a0849c49763c73a5f07cfbc97c9ca
kaushalendra-pandey <67643722+kaushalendra-pandey@users.noreply.github.com>
2022-07-10 18:59:31 +0530

    html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-export const smallTalk = () => {
-    console.log("Is this getting called new")
-}
-
-
-
+const para = document.createElement("p");
+const node = document.createTextNode("This is new.");
+para.appendChild(node);
