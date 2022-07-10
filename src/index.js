30272541daabcafb0db66ee04c9dd63e7f3325a3
kaushalendra-pandey <67643722+kaushalendra-pandey@users.noreply.github.com>
2022-07-10 19:05:44 +0530

    uodates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 const para = document.createElement("p");
 const node = document.createTextNode("This is new.");
-para.appendChild(node);
+
+const ele = document.getElementById("time)
+ele.appendChild(node);
